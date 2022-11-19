a231a4b8771b2a59cfad238241640839d2d62a73
Pragya <pragyaprashar490@gmail.com>
2022-11-19 17:27:33 -0500

    changes

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,23 +1,109 @@
 export default[
+    // {
+    //     "_id": 123,
+    //     "topic": "Space", "username": "SpaceX",
+    //     "handle": "@spacex", "time": "2h", "image": "spacex.png",
+    //     "title": "SpaceX's Mission",
+    //     "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+    //     "liked": true, "likes": 2345,
+    //     "replies": 123, "retuits": 432
+    //
+    //
+    // },
+    // {
+    //     "_id": 234,
+    //     "topic": "Traffic", "username": "The Boring Company",
+    //     "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
+    //     "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+    //     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+    //     "liked": true, "likes": 2345,
+    //     "replies": 123, "retuits": 432
+    //
+    // },
     {
         "_id": 123,
-        "topic": "Space", "username": "SpaceX",
-        "handle": "@spacex", "time": "2h", "image": "spacex.png",
-        "title": "SpaceX's Mission",
+        "avatarIcon":"../images/space_icon.jpg",
+        "topic": "Web Development",
+        "userName": "ReactJS",
+        "username": "SpaceX",
+        "time": "2h",
+        "title": "React.js is a component based front end library that makes it very easy to build Single Page Application or SPAs",
+        "image": "../images/react.png",
+        "tagline":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "tweets":"",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-        "liked": true, "likes": 2345,
-        "replies": 123, "retuits": 432
+        "liked": true,
+        "replies": 123,
+        "retuits": 432,
+        "likes": 2345,
+        "handle": "@spaceX",
 
+        "comment":"4.2K",
+        "retweet":"3.5K",
+        "heart":"37.5K"
 
     },
     {
-        "_id": 234,
-        "topic": "Traffic", "username": "The Boring Company",
-        "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
-        "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
-        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-        "liked": true, "likes": 2345,
-        "replies": 123, "retuits": 432
-
+        "_id": 124,
+        "avatarIcon":"../images/newyorkpost_icon.png",
+        "topic": "",
+        "userName": "JavaScript",
+        "username": "Elon Musk",
+        "time": "2h",
+        "title": "JavaScript is a programming language that can run on browsers as well as desktops",
+        "image": "../images/js.png",
+        "tagline":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "tweets": "123K Tuits",
+        "liked": true,
+        "replies": 123,
+        "retuits": 432,
+        "likes": 2345,
+        "handle": "@spaceX",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "comment":"4.2K",
+        "retweet":"3.5K",
+        "heart":"37.5K"
     },
+    {
+        "_id": 125,
+        "avatarIcon":"../images/space_icon.jpg",
+        "topic": "Web Development",
+        "time": "2h",
+        "userName":"jQuery",
+        "username": "SpaceX",
+        "title": "",
+        "image": "../images/jquery.png",
+        "tagline":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "tweets": "123K Tuits",
+        "liked": true,
+        "replies": 123,
+        "retuits": 432,
+        "likes": 2345,
+        "handle": "@spaceX",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "comment":"4.2K",
+        "retweet":"3.5K",
+        "heart":"37.5K"
+    },
+    {
+        "_id": 126,
+        "avatarIcon":"../images/newyorkpost_icon.png",
+        "topic": "Web Development",
+        "userName": "NodeJS",
+        "username": "Elon Musk",
+        "time": "2h",
+        "title": "",
+        "image": "../images/node_img.png",
+        "tagline":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "tweets": "123K Tuits",
+        "liked": true,
+        "replies": 123,
+        "retuits": 432,
+        "likes": 2345,
+        "handle": "@spaceX",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "comment":"4.2K",
+        "retweet":"3.5K",
+        "heart":"37.5K"
+    }
 ];
