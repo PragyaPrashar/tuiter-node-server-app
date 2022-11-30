a68d768cf49c058cbe20993fb3ed26ca78abe718
Pragya <pragyaprashar490@gmail.com>
2022-11-29 21:20:02 -0500

    assignment 9 changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import UsersController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 // mongoose.connect('mongodb://localhost:27017/tuiter');
-const MONGO_ATLAS='mongodb+srv://pragya04:JesusRekha04@cluster0.y8ptkfm.mongodb.net/tuiter?retryWrites=true&w=majority';
+const MONGO_ATLAS='mongodb+srv://pragya04:iamthebest@cluster0.y8ptkfm.mongodb.net/tuiter?retryWrites=true&w=majority';
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING === undefined? MONGO_ATLAS: process.env.DB_CONNECTION_STRING;
 
 const MONGO_LOCAL='mongodb://localhost:27017/tuiter';
